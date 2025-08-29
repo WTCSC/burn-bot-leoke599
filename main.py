2b91351c637071968c1a7937681639c5713877bc
leoke599 <leoke073@gmail.com>
2025-08-29 10:08:13 -0600

    added comments

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+# Roboburn - A playful script that asks for user input and responds with humorous roasts.
+
+# Get user inputs
 name = input("What's your name? ")
 age = int(input("How old are you? "))
 interest = input("What's your favorite hobby? ")
 job = input("What's your dream job? ")
 movie = input("What's your favorite movie? ")
 
+# Print humorous roasts based on user inputs
 print(f"Hello, {name}. You're {age} years old? Damn! You're an unc. Why didn't you buy bitcoin 30 years ago?")
 print(f"Me personally, I would not get caught {interest}. It's childish and boring. That's just me personally though.")
 print(f"Becoming a {job} is a terrible idea. I bet you can't even get it. Maybe you should put the fries in the bag instead.")
