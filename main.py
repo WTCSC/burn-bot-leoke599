50ccb501eb58301f3b0bc444a6d64d18d42f1d6d
leoke599 <leoke073@gmail.com>
2025-08-29 10:02:36 -0600

    Completed roboburn

---
+++ main.py
@@ -0,0 +1,11 @@
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+interest = input("What's your favorite hobby? ")
+job = input("What's your dream job? ")
+movie = input("What's your favorite movie? ")
+
+print(f"Hello, {name}. You're {age} years old? Damn! You're an unc. Why didn't you buy bitcoin 30 years ago?")
+print(f"Me personally, I would not get caught {interest}. It's childish and boring. That's just me personally though.")
+print(f"Becoming a {job} is a terrible idea. I bet you can't even get it. Maybe you should put the fries in the bag instead.")
+print(f"{movie} is a terrible movie. I can't believe you like that. You must have the worst taste in movies.")
+print("Thanks for the info. It's being sold right now to the ops")
